1c6ee902dc2762cff46a4af1f15a1ec84fec268c
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-06-04 21:58:41 -0300

    camera

---
+++ CoinDetector/CoinDetector/App/CoinDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinDetectorApp.swift
+//  CoinDetector
+//
+//  Created by Eduardo on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
+    }
+}
